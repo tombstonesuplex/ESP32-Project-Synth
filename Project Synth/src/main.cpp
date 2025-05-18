e2e2d6eb1865c0f58c9a80e86f7a083c82d72813
tombstonesuplex <wsmi448@aucklanduni.ac.nz>
2025-05-18 21:51:03 +1200

    sets up bluetooth controller

--- Project Synth/src/main.cpp
+++ Project Synth/src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 
 #include <Waves.h>
+#include <AudioPipeline.h>
+#include <BluetoothController.h>
+#include <USBCController.h>
 
 // put function declarations here:
-int myFunction(int, int);
+extern void initialiseAudioPipline();
+USBCController usbcController;
 
 // can only run in three scenarios:
     // 1. Power on -> Cold Boot from ROM loads firmware from external SPI flash
     // 2. Reset by pressing EN/RST button or ESP.restart() -> Warm Boot from ROM loads firmware from external SPI flash
     // 3. After uploading new firmware -> bootloader auto-resets ESP32 to run new firmware from external SPI flash
+// Declare usbcController globally so it is accessible in both setup() and loop()
 void setup() {
-  int result = myFunction(2, 3);
+    usbcController.setup();
+
+    // sets up the audio pipline via ESP32-A2DB and Audio Tools Libraries
+    BluetoothController bluetoothController = BluetoothController("Tronsmart Trip");
+    bluetoothController.setup();
+
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+    usbcController.poll();
 }
 
