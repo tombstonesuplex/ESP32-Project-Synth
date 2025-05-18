1e978da6db098e4e0db27d10318d66808ea039a5
tombstonesuplex <wsmi448@aucklanduni.ac.nz>
2025-05-18 21:50:26 +1200

    header files

---
+++ Project Synth/include/AudioPipeline.h
@@ -0,0 +1,13 @@
+#ifndef AUDIOPIPELINE_H
+#define AUDIOPIPELINE_H
+
+#include <math.h>
+#include <string>
+#include <Waves.h>
+
+// Declare the data callback function used by BluetoothA2DPSource
+int32_t get_sound_data(uint8_t *data, int32_t byteCount);
+
+// Setup function to start Bluetooth audio streaming
+
+#endif // AUDIOPIPELINE_H
